38ef5cd30b8c478058a788ef29cc4981552064da
Mr. WebWizard <142308196+sahilkhan8786@users.noreply.github.com>
2024-08-17 10:45:24 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -16,17 +16,17 @@ app.use("/api/orders/", orderRoute);
 
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+// if (process.env.NODE_ENV === "production") {
+//   // app.use(express.static(path.join(__dirname, "/client/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running....");
-  });
-}
+//   // app.get("*", (req, res) =>
+//   //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+//   // );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running....");
+//   });
+// }
 
 const PORT = process.env.PORT || 5000;
 
